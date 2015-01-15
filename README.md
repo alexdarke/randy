a2c3e8b30ef268ccbfcc26389d4c49b5ba77ee1b
Alex Darke <adarke@gmail.com>
2015-01-15 07:46:18 -0600

    01152015.01  reworked as a python script

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # randy
 really stupid script to randomize a sequential list and then randomly pick 1 of the lines
+
+v.0.0.2  Changed from bash script to python. Because reasons.
---
+++ randy.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import random, argparse
+
+parser = argparse.ArgumentParser(description='Script to randomize the lines in a file and then pick one randomly.')
+parser.add_argument('-i','--input', help='Input file name',required=True)
+args = parser.parse_args()
+
+with open(args.input,'r') as source:
+    data = [ (random.random(), line) for line in source ]
+data.sort()
+with open('randomfile','w') as target:
+    for _, line in data:
+        target.write( line )
+
+with open('randomfile') as f:
+    lines = random.sample(f.readlines(),1)
+
+print lines
